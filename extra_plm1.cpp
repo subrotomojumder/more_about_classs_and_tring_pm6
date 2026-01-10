0ef68637842ebf745e0efacf778d15c2db98f3e0
Janardhan-Majumder <janardhan.md03@gmail.com>
2026-01-10 19:44:37 +0600

    pracetice extra plm

---
+++ extra_plm1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string word;
+    while (ss >> word)
+    {
+        reverse(word.begin(), word.end());
+        cout << word << " ";
+    }
+    return 0;
+}
